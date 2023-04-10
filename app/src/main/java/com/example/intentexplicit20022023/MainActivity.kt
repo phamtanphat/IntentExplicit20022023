bf6f1e6dec104dfc1688df384aa75686d01db16b
Phat <phatdroid94@gmail.com>
2023-04-10 20:18:17 +0700

    string resource

--- app/src/main/java/com/example/intentexplicit20022023/MainActivity.kt
+++ app/src/main/java/com/example/intentexplicit20022023/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.intentexplicit20022023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvScore: TextView
+    private lateinit var imgRandom: ImageView
+    private lateinit var imgPick: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvScore = findViewById(R.id.text_view_score)
+        imgRandom = findViewById(R.id.image_view_random)
+        imgPick = findViewById(R.id.image_view_pick)
     }
 }
