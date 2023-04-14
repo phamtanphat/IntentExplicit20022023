3d3440a11c2a0faaa18efea050e2809f07c153a7
Phat <phatdroid94@gmail.com>
2023-04-14 20:26:00 +0700

    handle

--- app/src/main/java/com/example/intentexplicit20022023/ListAnimalActivity.kt
+++ app/src/main/java/com/example/intentexplicit20022023/ListAnimalActivity.kt
@@ -1,11 +1,11 @@
 package com.example.intentexplicit20022023
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import kotlin.math.ceil
 
 class ListAnimalActivity : AppCompatActivity() {
 
@@ -21,19 +21,22 @@ class ListAnimalActivity : AppCompatActivity() {
         arrNameAnimals = resources.getStringArray(R.array.array_animal).toList()
 
         val totalColumn = 3
-        val totalRow = 6
+        val totalRow = ceil((arrNameAnimals.size / 3.0f)).toInt()
 
-        for (row in 0 until totalRow) {
-            val tableRow = TableRow(this)
-            for (column in 0 until totalColumn) {
-                val image = ImageView(this)
-                val nameAnimal = arrNameAnimals[((totalColumn * row) + column)]
-                val imageResource = resources.getIdentifier(nameAnimal, "drawable", packageName)
-                image.setImageResource(imageResource)
-                tableRow.addView(image)
+        if (arrNameAnimals.isNotEmpty()) {
+            Loop1@for (row in 0 until totalRow) {
+                val tableRow = TableRow(this)
+                for (column in 0 until totalColumn) {
+                    val index = ((totalColumn * row) + column)
+                    if (index >= arrNameAnimals.size) break@Loop1
+                    val image = ImageView(this)
+                    val nameAnimal = arrNameAnimals[index]
+                    val imageResource = resources.getIdentifier(nameAnimal, "drawable", packageName)
+                    image.setImageResource(imageResource)
+                    tableRow.addView(image)
+                }
+                tableLayout.addView(tableRow)
             }
-            tableLayout.addView(tableRow)
         }
     }
-
 }
