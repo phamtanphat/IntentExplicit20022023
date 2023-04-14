ea89f4f9b61cafb6d7d11115af4e1f706800c6d2
Phat <phatdroid94@gmail.com>
2023-04-14 21:30:15 +0700

    finish

--- app/src/main/java/com/example/intentexplicit20022023/MainActivity.kt
+++ app/src/main/java/com/example/intentexplicit20022023/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentexplicit20022023
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imgRandom: ImageView
     private lateinit var imgPick: ImageView
     private lateinit var arrNameAnimals: List<String>
+    private var resourceRandom = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +35,21 @@ class MainActivity : AppCompatActivity() {
     private fun eventView() {
         imgPick.setOnClickListener {
             val intent = Intent(this@MainActivity, ListAnimalActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
+        }
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val resourcePick = result.data?.getIntExtra("resource", -1)
+            if (resourcePick == null || resourcePick == -1) return@registerForActivityResult
+            imgPick.setImageResource(resourcePick)
+            if (resourceRandom == resourcePick) {
+                Toast.makeText(this, "Chính xác", Toast.LENGTH_SHORT).show()
+                randomImage()
+            } else {
+                Toast.makeText(this, "Bạn đã chọn sai", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -51,7 +69,7 @@ class MainActivity : AppCompatActivity() {
         arrNameAnimals = resources.getStringArray(R.array.array_animal).toList()
         val indexRandom = Random.nextInt(arrNameAnimals.size)
         val nameAnimalRandom = arrNameAnimals[indexRandom]
-        val imageResource = resources.getIdentifier(nameAnimalRandom, "drawable", packageName)
-        imgRandom.setImageResource(imageResource)
+        resourceRandom = resources.getIdentifier(nameAnimalRandom, "drawable", packageName)
+        imgRandom.setImageResource(resourceRandom)
     }
 }
