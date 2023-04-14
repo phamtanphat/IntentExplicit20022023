6a06fab44dcd45b8b399181ba3a64c346ffce031
Phat <phatdroid94@gmail.com>
2023-04-14 21:13:33 +0700

    return value

--- app/src/main/java/com/example/intentexplicit20022023/ListAnimalActivity.kt
+++ app/src/main/java/com/example/intentexplicit20022023/ListAnimalActivity.kt
@@ -1,12 +1,15 @@
 package com.example.intentexplicit20022023
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.math.ceil
 
 class ListAnimalActivity : AppCompatActivity() {
@@ -20,7 +23,7 @@ class ListAnimalActivity : AppCompatActivity() {
 
         tableLayout = findViewById(R.id.table_layout)
 
-        arrNameAnimals = resources.getStringArray(R.array.array_animal).toList()
+        arrNameAnimals = resources.getStringArray(R.array.array_animal).toList().shuffled()
 
         val totalColumn = 3
         val totalRow = ceil((arrNameAnimals.size / 3.0f)).toInt()
@@ -40,6 +43,13 @@ class ListAnimalActivity : AppCompatActivity() {
                     val nameAnimal = arrNameAnimals[index]
                     val imageResource = resources.getIdentifier(nameAnimal, "drawable", packageName)
                     image.setImageResource(imageResource)
+                    image.setOnClickListener {
+                        val intent = Intent(this@ListAnimalActivity, MainActivity::class.java)
+                        val resource = resources.getIdentifier(arrNameAnimals[index], "drawable", packageName)
+                        intent.putExtra("resource", resource)
+                        setResult(RESULT_OK, intent)
+                        finish()
+                    }
                     tableRow.addView(image)
                 }
                 tableLayout.addView(tableRow)
